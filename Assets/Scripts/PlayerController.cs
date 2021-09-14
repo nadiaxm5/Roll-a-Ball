43b3df01b107fd628c5b9b2f9e4c9234c76ba1aa
Namomi5 <nadiamm0910@gmail.com>
2021-09-14 15:52:43 +0200

    colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,4 +30,9 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement*speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+    }
 }
